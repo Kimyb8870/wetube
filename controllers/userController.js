2139ef78b0ab2ee3c1e7d3ae11876b64cc4cc09b
kimyb8870 <qhd0084@gmail.com>
2021-01-26 08:11:18 +0900

    Set: basic setting of nodeJS

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+export const login = (req, res) => res.send("Login");
+export const join = (req, res) => res.send("Join");
+export const logout = (req, res) => res.send("Logout");
+
+export const users = (req, res) => res.send("users");
+export const userDetail = (req, res) => res.send("user detail");
+export const editProfile = (req, res) => res.send("edit profile");
+export const changePassword = (req, res) => res.send("change password");
